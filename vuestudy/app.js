8049ed6f5f9c9d7059f69dba28ec7f5b7d33f456
noheunmyoung <humance@gmail.com>
2020-01-15 00:47:56 +0900

    스터디3일차

--- vuestudy/app.js
+++ vuestudy/app.js
@@ -1,3 +1,4 @@
+// 실습1
 Vue.component('todo-footer', {
     template: '<p>This is another global child component</p>'
 });
@@ -7,12 +8,36 @@ var cmp = {
 };
 
 var app = new Vue({
-    el: '#app4',
+    el: '#app',
     data: {
         message: 'this is parent component'
     },
     components: {
         'todo-list': cmp
-    }
-
+    } 
 });
+
+
+// 실습2
+Vue.component('sibling-component', {
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>' 
+});
+
+Vue.component('child-component', {
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>' 
+}); 
+
+var app = new Vue({
+    el: '#app1',
+    data: {
+        message: 'Hello Vue! passed from Parent component1',
+        anotherMessage: 'Hello Vue! passed from Parent component2'
+    }
+});
+
+
+
+ 
+
