b43452f3110bf588ce5695c9c60d497737eff859
noh eunmyoung <humance@nate.com>
2022-03-26 11:47:40 +0900

    변경사항 저장

---
+++ nuxt-project/components/comp-sample/compp2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div> 
+    1
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+@Component({
+  components: {},
+  layout: 'nohead', 
+}) 
+export default class extends Vue {}
+</script>
+
+<style lang="scss"></style>
